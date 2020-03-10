a50d5acb11cb9a52ccf803c0e484f6dae2d483de
Vaughn Dice <vadice@microsoft.com>
2020-03-10 10:25:15 -0700

    update brigade.js with brigade-utils helpers
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- brigade.js
+++ brigade.js
@@ -1,4 +1,5 @@
 const { events, Job, Group } = require("brigadier");
+const { Check } = require("@brigadecore/brigade-utils");
 
 const projectName = "cnab-spec";
 
@@ -13,8 +14,8 @@ events.on("exec", (e, p) => {
 events.on("check_suite:requested", runSuite);
 events.on("check_suite:rerequested", runSuite);
 events.on("check_run:rerequested", runSuite);
-events.on("issue_comment:created", handleIssueComment);
-events.on("issue_comment:edited", handleIssueComment);
+events.on("issue_comment:created", (e, p) => Check.handleIssueComment(e, p, runSuite));
+events.on("issue_comment:edited", (e, p) => Check.handleIssueComment(e, p, runSuite));
 events.on("push", (e, p) => {
   if (e.revision.ref.startsWith("refs/tags/")) {
     return publish(e, p).run();
@@ -68,7 +69,7 @@ function publish(e, p) {
 // Here we can add additional Check Runs, which will run in parallel and
 // report their results independently to GitHub
 function runSuite(e, p) {
-  return runValidation(e, p)
+  return runValidation(e, p, "validate")
   .then(() => {
     if (e.revision.ref == "master") {
       validateURL(e, p).run();
@@ -78,100 +79,8 @@ function runSuite(e, p) {
 }
 
 // runValidation is a Check Run that is ran as part of a Checks Suite
-function runValidation(e, p) {
-  // Create Notification object (which is just a Job to update GH using the Checks API)
-  var note = new Notification(`validation`, e, p);
-  note.conclusion = "";
-  note.title = "Run Validation";
-  note.summary = "Running the schema validation for " + e.revision.commit;
-  note.text = "Ensuring all bundle.json files adhere to json schema specs"
-
-  // Send notification, then run, then send pass/fail notification
-  return notificationWrap(validate(e, p), note)
-}
-
-// handleIssueComment handles an issue_comment event, parsing the comment
-// text and determining whether or not to trigger a corresponding action
-function handleIssueComment(e, p) {
-  if (e.payload) {
-    payload = JSON.parse(e.payload);
-
-    // Extract the comment body and trim whitespace
-    comment = payload.body.comment.body.trim();
-
-    // Here we determine if a comment should provoke an action
-    switch(comment) {
-    case "/brig run":
-      return runSuite(e, p);
-    default:
-      console.log(`No applicable action found for comment: ${comment}`);
-    }
-  }
-}
-
-// A GitHub Check Suite notification
-class Notification {
-  constructor(name, e, p) {
-    this.proj = p;
-    this.payload = e.payload;
-    this.name = name;
-    this.externalID = e.buildID;
-    this.detailsURL = `https://brigadecore.github.io/kashti/builds/${ e.buildID }`;
-    this.title = "running check";
-    this.text = "";
-    this.summary = "";
-
-    // count allows us to send the notification multiple times, with a distinct pod name
-    // each time.
-    this.count = 0;
-
-    // One of: "success", "failure", "neutral", "cancelled", or "timed_out".
-    this.conclusion = "neutral";
-  }
-
-  // Send a new notification, and return a Promise<result>.
-  run() {
-    this.count++
-    var j = new Job(`${ this.name }-${ this.count }`, "brigadecore/brigade-github-check-run:v0.1.0");
-    j.env = {
-      CHECK_CONCLUSION: this.conclusion,
-      CHECK_NAME: this.name,
-      CHECK_TITLE: this.title,
-      CHECK_PAYLOAD: this.payload,
-      CHECK_SUMMARY: this.summary,
-      CHECK_TEXT: this.text,
-      CHECK_DETAILS_URL: this.detailsURL,
-      CHECK_EXTERNAL_ID: this.externalID
-    }
-    return j.run();
-  }
-}
-
-// Helper to wrap a job execution between two notifications.
-async function notificationWrap(job, note, conclusion) {
-  if (conclusion == null) {
-    conclusion = "success"
-  }
-  await note.run();
-  try {
-    let res = await job.run()
-    const logs = await job.logs();
-
-    note.conclusion = conclusion;
-    note.summary = `Task "${ job.name }" passed`;
-    note.text = note.text = "```" + res.toString() + "```\nTest Complete";
-    return await note.run();
-  } catch (e) {
-    const logs = await job.logs();
-    note.conclusion = "failure";
-    note.summary = `Task "${ job.name }" failed for ${ e.buildID }`;
-    note.text = "```" + logs + "```\nFailed with error: " + e.toString();
-    try {
-      return await note.run();
-    } catch (e2) {
-      console.error("failed to send notification: " + e2.toString());
-      console.error("original error: " + e.toString());
-      return e2;
-    }
-  }
+function runValidation(e, p, jobFunc) {
+  var check = new Check(e, p, jobFunc(),
+    `https://brigadecore.github.io/kashti/builds/${e.buildID}`);
+  return check.run();
 }
