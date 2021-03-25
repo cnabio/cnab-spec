17260ba0bfdb529333a2113f50eb945cfe0ea159
Carolyn Van Slyck <me@carolynvanslyck.com>
2021-03-25 10:15:26 -0500

    Include installation state schema in validation
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -8,49 +8,63 @@
 set -eou pipefail
 
 # Test all of the bundle files against the bundle schema
-for json in $(ls -1 examples/*-bundle.json); do
+for json in $(ls -1 examples/101*-bundle.json); do
   schema="schema/bundle.schema.json"
   echo "Testing json '$json' against schema '$schema'"
   ajv test -s $schema -d $json --valid -r schema/definitions.schema.json
 done
 
 # Test all of the claim files against the claim schema.
-for json in $(ls -1 examples/*-claim.json); do
+for json in $(ls -1 examples/400*-claim.json); do
   schema="schema/claim.schema.json"
   echo "Testing json '$json' against schema '$schema'"
   ajv test -s $schema -d $json --valid -r schema/bundle.schema.json -r schema/definitions.schema.json
 done
 
 # Test all of the claim result files against the claim result schema.
-for json in $(ls -1 examples/*-claim-result.json); do
+for json in $(ls -1 examples/400*-claim-result.json); do
   schema="schema/claim-result.schema.json"
   echo "Testing json '$json' against schema '$schema'"
   ajv test -s $schema -d $json --valid -r schema/definitions.schema.json
 done
 
+# Test all of the claim files against the installation state schema.
+for json in $(ls -1 examples/401*-claim.json); do
+  schema="schema/claim.v2.schema.json"
+  echo "Testing json '$json' against schema '$schema'"
+  ajv test -s $schema -d $json --valid -r schema/bundle.schema.json -r schema/definitions.schema.json
+done
+
+# Test all of the claim result files against the claim result schema.
+for json in $(ls -1 examples/401*-claim-result.json); do
+  schema="schema/claim-result.v2.schema.json"
+  echo "Testing json '$json' against schema '$schema'"
+  ajv test -s $schema -d $json --valid -r schema/definitions.schema.json
+done
+
 # Test all of the dependency files against the dependencies schema
-for json in $(ls -1 examples/*-dependencies.json); do
+for json in $(ls -1 examples/500*-dependencies.json); do
   schema="schema/dependencies.schema.json"
   echo "Testing json '$json' against schema '$schema'"
   ajv test -s $schema -d $json --valid
 done
 
 # Test all of the parameter sources files against the parameter sources schema
-for json in $(ls -1 examples/*-parameter-sources.json); do
+for json in $(ls -1 examples/810*-parameter-sources.json); do
   schema="schema/parameter-sources.schema.json"
   echo "Testing json '$json' against schema '$schema'"
   ajv test -s $schema -d $json --valid
 done
 
 # Test all of the relocation mapping files against the relocation mapping schema.
-for json in $(ls -1 examples/*-relocation-mapping.json); do
+for json in $(ls -1 examples/103*-relocation-mapping.json); do
   schema="schema/relocation-mapping.schema.json"
   echo "Testing json '$json' against schema '$schema'"
   ajv test -s $schema -d $json --valid
 done
 
 # Test all of the status files against the status schema.
-for json in $(ls -1 examples/*-status.json); do
+for json in $(ls -1 examples/804*-status.json); do
   schema="schema/status.schema.json"
   echo "Testing json '$json' against schema '$schema'"
   ajv test -s $schema -d $json --valid -r schema/bundle.schema.json
